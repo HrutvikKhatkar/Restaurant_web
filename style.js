ada74c76005a58b9808267abae0be5b26b939318
Lizhaoqi <2652376648@qq.com>
2024-11-11 18:10:54 +0800

    Free color switch

--- style.js
+++ style.js
@@ -6,3 +6,11 @@ document.querySelectorAll('.wcu-card').forEach(card => {
         card.querySelector('.popup').style.display = 'block';
     });
 });
+function toggleDarkMode() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("bg-white-mode");
+  navbar.classList.toggle("bg-dark-mode");
+  
+  document.body.classList.toggle("dark-mode");
+  
+}
